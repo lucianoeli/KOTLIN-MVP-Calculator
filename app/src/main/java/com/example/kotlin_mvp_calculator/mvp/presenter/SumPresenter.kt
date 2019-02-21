62e070860dbc1ce770dcae1de1330d52513c74b2
lucho eli <luciano.elissondo@globant.com>
2019-02-21 14:35:02 -0300

    initial structure

---
+++ app/src/main/java/com/example/kotlin_mvp_calculator/mvp/presenter/SumPresenter.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_mvp_calculator.mvp.presenter
+
+class SumPresenter {
+}
---
+++ app/src/main/java/com/example/kotlin_mvp_calculator/rx/Events.kt
@@ -0,0 +1,8 @@
+package com.example.kotlin_mvp_calculator.rx
+
+
+object EventTypes {
+    const val INPUT_EVENT = 0
+    const val RESET_INPUT_EVENT = 1
+    const val SHOW_RESULT_EVENT = 2
+}
