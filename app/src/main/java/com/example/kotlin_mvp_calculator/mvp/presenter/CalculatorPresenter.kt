ab4c066ee00908746d986f90496eb8f00baf7a0a
lucho eli <luciano.elissondo@globant.com>
2019-02-25 11:53:46 -0300

    removing braces

--- app/src/main/java/com/example/kotlin_mvp_calculator/mvp/presenter/CalculatorPresenter.kt
+++ app/src/main/java/com/example/kotlin_mvp_calculator/mvp/presenter/CalculatorPresenter.kt
@@ -50,76 +50,24 @@ class CalculatorPresenter(val model: CalculatorModel, val view: CalculatorView) 
         compositeDisposable.add(
             view.viewEventObservable.subscribe { clickEvent ->
                 when (clickEvent) {
-                    RESET_EVENT -> {
-                        model.reset()
-                    }
-
-                    DELETE_EVENT -> {
-                        model.delete()
-                    }
-                    RESULT_EVENT -> {
-                        model.operate()
-                    }
-
-                    ZERO_EVENT -> {
-                        model.inputValue(ZERO)
-                    }
-
-                    ONE_EVENT -> {
-                        model.inputValue(ONE)
-                    }
-
-                    TWO_EVENT -> {
-                        model.inputValue(TWO)
-                    }
-
-                    THREE_EVENT -> {
-                        model.inputValue(THREE)
-                    }
-
-                    FOUR_EVENT -> {
-                        model.inputValue(FOUR)
-                    }
-
-                    FIVE_EVENT-> {
-                        model.inputValue(FIVE)
-                    }
-
-                    SIX_EVENT -> {
-                        model.inputValue(SIX)
-                    }
-
-                    SEVEN_EVENT -> {
-                        model.inputValue(SEVEN)
-                    }
-
-                    EIGHT_EVENT-> {
-                        model.inputValue(EIGHT)
-                    }
-
-                    NINE_EVENT-> {
-                        model.inputValue(NINE)
-                    }
-
-                    DOT_EVENT-> {
-                        model. inputValue(DOT)
-                    }
-
-                    OP_PLUS_EVENT -> {
-                        model.inputOp(PLUS)
-                    }
-
-                    OP_MIN_EVENT -> {
-                        model.inputOp(MINUS)
-                    }
-
-                    OP_MUL_EVENT -> {
-                        model.inputOp(MULTIPLY)
-                    }
-
-                    OP_DIV_EVENT -> {
-                        model.inputOp(DIVIDE)
-                    }
+                    RESET_EVENT -> model.reset()
+                    DELETE_EVENT -> model.delete()
+                    RESULT_EVENT -> model.operate()
+                    ZERO_EVENT -> model.inputValue(ZERO)
+                    ONE_EVENT -> model.inputValue(ONE)
+                    TWO_EVENT -> model.inputValue(TWO)
+                    THREE_EVENT -> model.inputValue(THREE)
+                    FOUR_EVENT -> model.inputValue(FOUR)
+                    FIVE_EVENT -> model.inputValue(FIVE)
+                    SIX_EVENT -> model.inputValue(SIX)
+                    SEVEN_EVENT -> model.inputValue(SEVEN)
+                    EIGHT_EVENT -> model.inputValue(EIGHT)
+                    NINE_EVENT -> model.inputValue(NINE)
+                    DOT_EVENT -> model.inputValue(DOT)
+                    OP_PLUS_EVENT -> model.inputOp(PLUS)
+                    OP_MIN_EVENT -> model.inputOp(MINUS)
+                    OP_MUL_EVENT -> model.inputOp(MULTIPLY)
+                    OP_DIV_EVENT -> model.inputOp(DIVIDE)
                 }
 
                 view.setText(model.getData())
